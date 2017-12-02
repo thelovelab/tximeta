269edf9f98db7d58b0026cc3062a414fe2245486
mikelove <michaelisaiahlove@gmail.com>
2017-12-02 08:32:22 -0500

    working on derived txomes

--- R/derived.R
+++ R/derived.R
@@ -16,8 +16,10 @@
 makeDerivedTxome <- function(indexDir, source, organism, version, genome, fasta, gtf, write=TRUE) {
   indexList <- fromJSON(file.path(indexDir,"header.json"))
   indexSeqHash <- indexList$value0$SeqHash
+  index <- basename(indexDir)
   # TODO this should be made to work for multiple fasta and GTF sources...
-  dt <- list(index_seq_hash=indexSeqHash,
+  dt <- list(index=index,
+             index_seq_hash=indexSeqHash,
              source=source,
              organism=organism,
              version=version,
@@ -29,12 +31,31 @@ makeDerivedTxome <- function(indexDir, source, organism, version, genome, fasta,
     message(paste("writing derivedTxome to", filename))
     write(toJSON(dt, pretty=TRUE), file=filename)
   }
-  dt
   
-  ## bfc <- BiocFileCache(".")
-  ## q <- bfcquery(bfc, "derivedTxomeTable")
-  ## bfccount(q)
-  ## savepath <- bfcnew(bfc, "derivedTxomeTable") 
-  ## loadpath <- bfcrpath(bfc, "derivedTxomeTable")
-  
+  bfc <- BiocFileCache(".")
+  q <- bfcquery(bfc, "derivedTxomeDF")
+
+  if (bfccount(q) == 0) {
+    message("saving derivedTxome in bfc (first time)")
+    savepath <- bfcnew(bfc, "derivedTxomeDF", ext="rds")
+    derivedTxomeDF <- data.frame(dt, stringsAsFactors=FALSE)
+    saveRDS(derivedTxomeDF, file=savepath)
+  } else {
+    loadpath <- bfcrpath(bfc, "derivedTxomeDF")
+    derivedTxomeDF <- readRDS(loadpath)
+    if (index %in% derivedTxomeDF$index) {
+      m <- match(index, derivedTxomeDF$index)
+      if (all(data.frame(dt) == derivedTxomeDF[m,,drop=FALSE])) {
+        message("derivedTxome is same as already in bfc")
+      } else {
+        message("derivedTxome was different than one in bfc, replacing")
+        derivedTxomeDF[m,] <- dt
+      }
+    } else {
+      message("saving derivedTxome in bfc")
+      derivedTxomeDF <- rbind(derivedTxomeDF, dt)
+    }
+  }
 }
+
+# need a loadDerivedTxome() which will share a lot of code with the above function
