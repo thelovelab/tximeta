61301f23b648b01b88de1537933919411e08b1df
mikelove <michaelisaiahlove@gmail.com>
2018-10-17 17:04:15 -0400

    adding support for inf reps

--- tests/testthat/test_tximeta.R
+++ tests/testthat/test_tximeta.R
@@ -20,11 +20,42 @@ test_that("tximeta works as expected", {
 
 test_that("tximeta can import kallisto", {
 
-  library(readr)
   dir <- system.file("extdata", package="tximportData")
   samples <- read.table(file.path(dir,"samples.txt"), header=TRUE)
   files <- file.path(dir,"kallisto", samples$run, "abundance.tsv.gz")
   coldata <- data.frame(files, names=paste0("sample",1:6))
   se <- tximeta(coldata, type="kallisto", txOut=TRUE)
+
+  # inferential replicates as well
+  library(SummarizedExperiment)
+  files <- file.path(dir,"kallisto_boot", samples$run, "abundance.h5")
+  coldata <- data.frame(files, names=paste0("sample",1:6))
+  se <- tximeta(coldata, type="kallisto", txOut=TRUE)
+  expect_true("infRep1" %in% assayNames(se))
+  se <- tximeta(coldata, type="kallisto", txOut=TRUE, varReduce=TRUE)
+  expect_true("variance" %in% assayNames(se))
+  
+})
+
+test_that("tximeta can import inferential replicates", {
+
+  # don't want to rely on internet connection for tests...
+  if (FALSE) {
+    library(SummarizedExperiment)
+    dir <- system.file("extdata", package="tximportData")
+    samples <- read.table(file.path(dir,"samples.txt"), header=TRUE)
+    files <- file.path(dir,"salmon_gibbs", samples$run, "quant.sf.gz")
+    coldata <- data.frame(files, names=paste0("sample",1:6))
+    se <- tximeta(coldata)
+    expect_true("infRep1" %in% assayNames(se))
+
+    se2 <- tximeta(coldata, varReduce=TRUE)
+    expect_true("variance" %in% assayNames(se))
+
+    gse <- summarizeToGene(se)
+    # TODO this doesn't work yet
+    # gse <- summarizeToGene(se, varReduce=TRUE)
+    
+  }
   
 })
