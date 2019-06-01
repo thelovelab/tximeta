f3a44fc5dca989e6f67492ae1f06d8e1b149dc46
mikelove <michaelisaiahlove@gmail.com>
2019-06-01 08:36:19 -0700

    forgot to add new R and man files

---
+++ R/addExons.R
@@ -0,0 +1,58 @@
+#' Add exons to rowRanges of a transcript-level SummarizedExperiment
+#'
+#' After running \code{tximeta}, the \code{SummarizedExperiment} output
+#' will have \code{GRanges} representing the transcript locations
+#' attached as \code{rowRanges} to the object. These provide the
+#' start and end of the transcript in the genomic coordiantes, and
+#' strand information. However, the exonic locations are not provided.
+#' This function, \code{addExons}, swaps out the \code{GRanges}
+#' with a \code{GRangesList}, essentially a list along the rows of the
+#' \code{SummarizedExperiment}, where each element of the list is a
+#' \code{GRanges} providing the locations of the exons for that transcript.
+#'
+#' This function is designed only for transcript-level objects.
+#' This "lack of a feature" reflects a belief on the part of the package author
+#' that it makes more sense to think about exons belonging to transcripts
+#' than to genes. For users desiring exonic information alongside
+#' gene-level objects, for example, which exons are associated with
+#' a particular gene, it is recommended to pull out the relevant
+#' \code{GRangesList} for the transcripts of this gene, while the object
+#' represents transcript-level data, such that the exons are still
+#' associated with transcripts. 
+#' 
+#' @param se the SummarizedExperiment
+#'
+#' @return a SummarizedExperiment
+#'
+#' @examples
+#'
+#' example(tximeta)
+#' se <- addExons(se)
+#' 
+#' @export
+addExons <- function(se) {
+
+  if (metadata(se)$level == "gene") {
+    stop("addExons() is design for transcript-level SummarizedExperiments, see ?addExons")
+  }
+  missingMetadata(se)
+  
+  txdb <- getTxDb(metadata(se)$txomeInfo)
+
+  message("obtaining exons from TxDb")
+  # TODO suppress warnings about out-of-bound ranges for now... how to pass this on
+  suppressWarnings({
+    exons <- exonsBy(txdb, by="tx", use.names=TRUE)
+  })
+
+  # check if all transcripts are present, and then subset
+  stopifnot(all(rownames(se) %in% names(exons)))
+  exons <- exons[rownames(se)]
+  stopifnot(all(rownames(se) == names(exons)))
+
+  # carry over the metadata from 'se'
+  mcols(exons) <- mcols(se)
+  # swap the rowRanges
+  rowRanges(se) <- exons
+  se
+}
