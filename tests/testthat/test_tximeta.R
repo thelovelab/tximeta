8bd0beccfc797e3c5a59ecb1f76f43afef8b7517
mikelove <michaelisaiahlove@gmail.com>
2022-03-23 11:58:11 -0400

    commenting out test code while alevin data is updated

--- tests/testthat/test_tximeta.R
+++ tests/testthat/test_tximeta.R
@@ -158,10 +158,10 @@ test_that("tximeta can import inferential replicates", {
 
 test_that("tximeta can import alevin", {
 
-  dir <- system.file("extdata", package="tximportData")
-  files <- file.path(dir,"alevin/neurons_900_v014/alevin/quants_mat.gz")
-  file.exists(files)
-  coldata <- data.frame(files, names="neurons")
+  ## dir <- system.file("extdata", package="tximportData")
+  ## files <- file.path(dir,"alevin/neurons_900_v014/alevin/quants_mat.gz")
+  ## file.exists(files)
+  ## coldata <- data.frame(files, names="neurons")
   #se <- tximeta(coldata, type="alevin")
   #se <- tximeta(coldata, type="alevin", dropInfReps=TRUE)
   #se <- tximeta(coldata, type="alevin", dropInfReps=TRUE, skipMeta=TRUE)
