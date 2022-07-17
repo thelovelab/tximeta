707541e80d886a8d40125e4d81a4ca150731313d
mikelove <michaelisaiahlove@gmail.com>
2022-07-16 22:40:21 -0400

    updating to gencode 41/M30 ensembl 107

--- inst/script/metatximeta.R
+++ inst/script/metatximeta.R
@@ -101,12 +101,12 @@ hashit <- function(source, organism, release, catNC=FALSE, ...) {
 # download method
 m <- "wget"
 
-i <- 40
+i <- 41
 hashit("GENCODE", "Homo sapiens", i, method=m)
-i <- 29
+i <- 30
 hashit("GENCODE", "Mus musculus", paste0("M",i), method=m)
 #
-i <- 106
+i <- 107
 hashit("Ensembl", "Homo sapiens", i, method=m)
 hashit("Ensembl", "Mus musculus", i, method=m)
 hashit("Ensembl", "Drosophila melanogaster", i, method=m)
