e5e34429808ce8759faa77d76525ef3dcce16f73
Mike Love <mikelove@users.noreply.github.com>
2025-06-25 13:48:28 -0400

    first commit to tximix

---
+++ R/tximix.R
@@ -0,0 +1,5 @@
+# this will be the start of a new function tximix()
+# this will allow separation of annotated and novel transcripts.
+# this will support quantification in the case of mixed
+# transcriptomes, including additions from de novo assembly,
+# as well as transgenes and spike-ins.
