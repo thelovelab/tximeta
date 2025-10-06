dc966131dc5a786707808bda82f42c071377406c
Mike Love <mikelove@users.noreply.github.com>
2025-10-06 08:13:37 -0400

    fix again

--- tests/testthat/test_mixed_reference.R
+++ tests/testthat/test_mixed_reference.R
@@ -95,8 +95,7 @@ test_that("importing oarfish with mixed reference works as expected", {
     indexName = "my_novel_txps",
     txpData = novel_gr,
     source = "novel", organism="Homo sapience", 
-    release="v1", genome="GRCh38",
-    prefer="txpdata"
+    release="v1", genome="GRCh38"
   )
 
 })
