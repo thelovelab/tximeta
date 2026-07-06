e5f5313c7db4631be39453252205341d981dc52f
Mike Love <mikelove@users.noreply.github.com>
2026-07-06 18:45:50 +0200

    fix bug in checkViaAHub

--- R/get_txome_txpdata.R
+++ R/get_txome_txpdata.R
@@ -107,7 +107,7 @@ getTxDb <- function(txomeInfo, useHub=TRUE, skipFtp=FALSE) {
       ensSrc <- srcName == "Ensembl"
       dbType <- if (ensSrc) "EnsDb" else "TxDb"
       if (useHub) {
-        txdb <- checkViaAHub(txomeInfo, srcName, ensSrc, dbType)
+        txdb <- checkViaAHub(txomeInfo, bfc, srcName, ensSrc, dbType)
         hubWorked <- !is.null(txdb)
       }
       # if check on AnnotationHub failed (or wasn't attempted)
@@ -140,7 +140,7 @@ locateTxDb <- function(txomeInfo, bfc) {
   q[q$rname==txdbName,]
 }
 
-checkViaAHub <- function(txomeInfo, srcName, ensSrc, dbType) {
+checkViaAHub <- function(txomeInfo, bfc, srcName, ensSrc, dbType) {
   # first check for database on AnnotationHub
   message(paste("useHub=TRUE: checking for", dbType, "via 'AnnotationHub'"))
   ah <- AnnotationHub()
